3e4de9652034b938a7cf43a02257f4e9c9993884
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-11-28 12:14:42 -0500

    initial commit

---
+++ ICE11A/Program.cs
@@ -0,0 +1,186 @@
+using System.Diagnostics;
+
+namespace ICE11A
+{
+        public enum FormType
+        {
+            Splash,
+            Start,
+            Selection,
+            Next,
+            Final,
+            About
+        }
+
+        public enum ToastType
+        {
+            Success,
+            Danger,
+            Warning,
+            Info
+        }
+
+    internal static class Program
+    {
+        public static SplashForm SplashForm;
+        public static StartForm StartForm;
+        public static SelectionForm SelectionForm;
+        public static NextForm NextForm;
+        public static FinalForm FinalForm;
+        public static AboutForm AboutForm;
+
+        public static List<Form> Forms;
+
+        private static bool IsExiting = false;
+
+        public static bool HasLoadedCharacter = false;
+
+        public static string DownloadsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),"Downloads");
+
+        [STAThread]
+        static void Main()
+        {
+
+            ApplicationConfiguration.Initialize();
+
+            SplashForm = new SplashForm();
+            StartForm = new StartForm();
+            SelectionForm = new SelectionForm();
+            NextForm = new NextForm();
+            FinalForm = new FinalForm();
+            AboutForm = new AboutForm();
+
+            Forms =
+            [
+                SplashForm,
+                StartForm,
+                SelectionForm,
+                NextForm,
+                FinalForm,
+                AboutForm
+            ];
+
+            Application.Run(SplashForm);
+        }
+
+        public static void ConfirmExit(FormClosingEventArgs e)
+        {
+            if (IsExiting)
+            {
+                return;
+            }
+
+            var result = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                IsExiting = true;
+                Application.Exit();
+            }
+        }
+
+        public static void SaveCharacter(string path)
+        {
+            using StreamWriter writer = new StreamWriter(path);
+
+            writer.WriteLine(Settings.Default.AGL);
+            writer.WriteLine(Settings.Default.STR);
+            writer.WriteLine(Settings.Default.VGR);
+            writer.WriteLine(Settings.Default.PER);
+            writer.WriteLine(Settings.Default.INT);
+            writer.WriteLine(Settings.Default.WIL);
+            writer.WriteLine(Settings.Default.CharacterName);
+            writer.WriteLine(Settings.Default.Species);
+            writer.WriteLine(Settings.Default.Career);
+        }
+
+        public static void LoadCharacter(string path)
+        {
+
+            try
+            {
+                // Check for file existence
+                if (!File.Exists(path))
+                {
+                    throw new FileNotFoundException("Character File does not exist");
+                }
+
+                // Check for empty file
+                FileInfo info = new FileInfo(path);
+                if (info.Length == 0)
+                {
+                    throw new FileFormatException("Character file is empty");
+                }
+
+                // Quick check for file validity by counting the number of lines
+                string[] lines = File.ReadAllLines(path);
+                if (lines.Length < Settings.Default.Properties.Count)
+                {
+                    throw new FileFormatException("Invalid Character file");
+                }
+
+                using StreamReader reader = new StreamReader(path);
+
+                var AGL = reader.ReadLine();
+                var STR = reader.ReadLine();
+                var VGR = reader.ReadLine();
+                var PER = reader.ReadLine();
+                var INT = reader.ReadLine();
+                var WIL = reader.ReadLine();
+                var CharacterName = reader.ReadLine();
+                var Species = reader.ReadLine();
+                var Career = reader.ReadLine();
+
+                if (AGL == null || STR == null || VGR == null || PER == null || INT == null || WIL == null ||
+                    CharacterName == null || Species == null || Career == null)
+                {
+                    throw new FileFormatException("Invalid Character file");
+                }
+
+                Settings.Default.AGL = AGL;
+                Settings.Default.STR = STR;
+                Settings.Default.VGR = VGR;
+                Settings.Default.PER = PER;
+                Settings.Default.INT = INT;
+                Settings.Default.WIL = WIL;
+                Settings.Default.CharacterName = CharacterName;
+                Settings.Default.Species = Species;
+                Settings.Default.Career = Career;
+            }
+            catch (FileNotFoundException e)
+            {
+                ShowToast("File Not Found: " + e.Message, ToastType.Danger);
+            }
+            catch (FileFormatException e)
+            {
+                ShowToast("Format Error: " + e.Message, ToastType.Danger);
+            }
+            catch (Exception e)
+            {
+                ShowToast("Error: " + e.Message, ToastType.Danger);
+            }
+        }
+
+        public static void ShowToast(string message, ToastType type = ToastType.Success)
+        {
+            const int padding = 20;
+
+            ToastForm toast = new ToastForm(message, type);
+
+            // Determine the area to center the toast in
+            Rectangle area = Form.ActiveForm.Bounds;
+
+            // Calculate the position to center the toast at the top of the area
+            int x = area.Left + (area.Width - toast.Width) / 2;
+            int y = area.Top + padding;
+
+            // Set the location of the toast and show it
+            toast.Location = new Point(x, y);
+            toast.TopMost = true;
+            toast.Show(Form.ActiveForm);
+        }
+    }
+}
---
+++ ICE11A/StartForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICE11A
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Program.ConfirmExit(e);
+        }
+
+        private void Button_New_Click(object sender, EventArgs e)
+        {
+            Program.Forms[(int)FormType.Selection].Show();
+            Hide();
+        }
+
+        /// <summary>
+        /// This event handler loads a character from a file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Load_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Load Character";
+            dialog.Filter = "Character Files (*.chr)|*.chr|All Files (*.*)|*.*";
+            dialog.InitialDirectory = Program.DownloadsFolder;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Program.LoadCharacter(dialog.FileName);
+                Program.HasLoadedCharacter = true;
+                Program.Forms[(int)FormType.Selection].Show();
+                Hide();
+            }
+        }
+
+        private void Button_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
